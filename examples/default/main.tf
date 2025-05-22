468a8a1feffd7b970ddc81f5b19fb68f5cb17a71
Karni Gupta <78787142+Karni-G@users.noreply.github.com>
2025-05-22 20:54:36 +0530

    module fix

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -78,7 +78,7 @@ module "key_vault" {
 
 data "azurerm_key_vault_secret" "test_secret" {
   name         = "test-secret"
-  key_vault_id = module.key_vault.secrets_resource_ids
+  key_vault_id = module.key_vault.id
   depends_on   = [module.key_vault]
 }
 
