1bfd7e12cdb1678163fe2932c3f843b029cbd035
Karni Gupta <78787142+Karni-G@users.noreply.github.com>
2025-05-23 18:56:41 +0530

    updated providers

--- terraform.tf
+++ terraform.tf
@@ -2,22 +2,22 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = ">= 1.12.0"
-    }
+    # azapi = {
+    #   source  = "azure/azapi"
+    #   version = ">= 1.12.0"
+    # }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.28.0, < 5.0.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.5.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.4.0"
-    }
+    # http = {
+    #   source  = "hashicorp/http"
+    #   version = ">= 3.5.0"
+    # }
+    # local = {
+    #   source  = "hashicorp/local"
+    #   version = ">= 2.4.0"
+    # }
     modtm = {
       source  = "azure/modtm"
       version = ">= 0.1.0"
@@ -26,9 +26,9 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5.0"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9.0"
-    }
+    # time = {
+    #   source  = "hashicorp/time"
+    #   version = ">= 0.9.0"
+    # }
   }
 }
