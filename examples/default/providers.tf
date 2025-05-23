adc03545815694f24065b7ad14855b7d34760c16
Karni Gupta <78787142+Karni-G@users.noreply.github.com>
2025-05-23 17:01:50 +0530

    updated provider.tf

---
+++ examples/default/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.28.0, < 5.0.0" # Latest version published a day ago [1](https://registry.terraform.io/providers/hashicorp/azurerm/latest)
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0, < 4.0" # Latest stable version
+    }
+    modtm = {
+      source  = "Azure/modtm"
+      version = "~> 0.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
